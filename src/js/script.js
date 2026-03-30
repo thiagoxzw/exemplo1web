03552de31244369b25093d38023709112b5f19a8
Thiago <thiagoslima123@gmail.com>
2026-03-30 09:15:40 -0300

    feat: Declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,49 @@
+// Declarações e variaveis
+// var let const
+
+var nome="fiap";
+console.log(nome);
+
+var nome="Teste"
+console.log(nome);
+
+if(true){
+    let apelido="Fiapinho";
+    console.log(apelido);
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros);
+// taxaJuros=1.5;
+
+var exemplo="ola-dev-var";
+console.log(typeof exemplo);
+
+
+let exemplo1="ola-dev-let";
+console.log(exemplo1);
+
+const exemplo2="ola-dev-const";
+console.log(exemplo2);
+
+let exemplo3= 10 + 1
+console.log(exemplo3);
+
+let exemplo4=true;
+console.log(exemplo4);
+
+let exemplo5={nome1:"teste"};
+console.log(exemplo5);
+
+let exemplo6=["Java","C#","PHP"];
+console.log(exemplo6);
+
+// type of dentro do console.log mostra qual o tipo de variavel
+
+let exemplo7;
+console.log(exemplo7);
+
+// valor vazio
+
+let exemplo8=null;
+console.log(exemplo8);
